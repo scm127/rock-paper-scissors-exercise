3bd79cf5b0538ade26e4dd17ca431ba75e8b7d53
scm127 <67708429+scm127@users.noreply.github.com>
2021-02-03 17:35:08 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,20 +1,21 @@
+print"----------------"
+
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
--------------------
+print"----------------"
 print("Please choose either 'rock', 'paper', or 'scissors':") 
 
 x=input("Please choose either 'Rock', 'Paper', or 'Scissors'")
-print(x)
+
+print("You chose: ", x)
+
 exit()
 
 
-print("You chose: 'rock'")
 
 
 print("The computer chose: 'paper'")
 
--------------------
 print("Oh, the computer won. It's ok.")
-------------------
 print("Thanks for playing. Please play again!")
 
 
