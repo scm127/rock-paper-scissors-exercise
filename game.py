6c5918e8ff4f626c276a8c31b3444f4afc1f8aab
scm127 <67708429+scm127@users.noreply.github.com>
2021-02-06 11:27:23 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,24 +1,39 @@
-print"----------------"
+import random
+from dotenv import load_dotenv
 
-print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
-print"----------------"
-print("Please choose either 'rock', 'paper', or 'scissors':") 
+load_dotenv()
+PLAYER_NAME=os.getenv("PLAYER_NAME", default="Player One")
 
-x=input("Please choose either 'Rock', 'Paper', or 'Scissors'")
+print("----------------")
+print("Welcome ", PLAYER_NAME, " to my Rock-Paper-Scissors game...")
+print("----------------")
 
-print("You chose: ", x)
+user_input= input("Please choose either 'rock', 'paper', or 'scissors' :")
+print("----------------")
+
+user_input.lower()
+rps= ["rock","paper","scissors"]
+if user_input not in rps:
+    print("Please enter a valid option and try again")
+    exit()
+
+print("You chose: ", user_input)
+
+
+computer_choice= random.choice(rps)
+print("The computer chose: " , computer_choice)
+
+
+
+if computer_choice==user_input:print("it's a tie")
+elif user_input == "paper" and computer_choice == "rock": print("You win! Congrats")
+elif user_input == "paper" and computer_choice == "scissors": print("Oh! The computer won, that's ok!")
+elif user_input == "rock" and computer_choice == "paper": print("Oh! The computer won, that's ok!")
+elif user_input == "rock" and computer_choice == "scissors": print("You win! Congrats")
+elif user_input == "scissors" and computer_choice == "paper": print("You win! Congrats")
+elif user_input == "scissors" and computer_choice == "rock": print("Oh! The computer won, that's ok!")
 
 exit()
 
 
 
-
-print("The computer chose: 'paper'")
-
-print("Oh, the computer won. It's ok.")
-print("Thanks for playing. Please play again!")
-
-
-
-
-print("Rock, Paper, Scissors, Shoot!")
