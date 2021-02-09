afe7a8e9aa3e33bd0df76038df1de9b8e2f5f442
scm127 <67708429+scm127@users.noreply.github.com>
2021-02-08 20:35:12 -0500

    Update game.py

--- game.py
+++ game.py
@@ -13,8 +13,7 @@ user_input= input("Please choose either 'rock', 'paper', or 'scissors' :")
 print("----------------")
 
 rps= ["rock","paper","scissors"]
-RPS= ["Rock","Paper","Scissors"]
-if user_input not in rps and RPS:
+if user_input not in rps:
     print("Please enter a valid option and try again")
     exit()
 
@@ -25,7 +24,6 @@ computer_choice= random.choice(rps)
 print("The computer chose: " , computer_choice)
 print("----------------")
 
-# I added the last 9 lines of the code to allow for an uppercase first letter becasue I could not get the lower() fuction to work properly. 
 
 if computer_choice==user_input:print("it's a tie")
 elif user_input == "paper" and computer_choice == "rock": print("You win! Congrats")
@@ -34,15 +32,7 @@ elif user_input == "rock" and computer_choice == "paper": print("Oh! The compute
 elif user_input == "rock" and computer_choice == "scissors": print("You win! Congrats")
 elif user_input == "scissors" and computer_choice == "paper": print("You win! Congrats")
 elif user_input == "scissors" and computer_choice == "rock": print("Oh! The computer won, that's ok!")
-elif user_input == "Paper" and computer_choice == "rock": print("You win! Congrats")
-elif user_input == "Paper" and computer_choice == "scissors": print("Oh! The computer won, that's ok!")
-elif user_input == "Rock" and computer_choice == "paper": print("Oh! The computer won, that's ok!")
-elif user_input == "Rock" and computer_choice == "scissors": print("You win! Congrats")
-elif user_input == "Scissors" and computer_choice == "paper": print("You win! Congrats")
-elif user_input == "Scissors" and computer_choice == "rock": print("Oh! The computer won, that's ok!")
-elif user_input == "Paper" and computer_choice == "paper": print("it's a tie")
-elif user_input == "Scissors" and computer_choice == "scissors": print("it's a tie")
-elif user_input == "Rock" and computer_choice == "rock": print("it's a tie")
+
 exit()
 
 
