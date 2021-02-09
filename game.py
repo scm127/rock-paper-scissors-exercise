c0f76327cc87d33d97db2e548445b284187a3c57
scm127 <67708429+scm127@users.noreply.github.com>
2021-02-08 20:21:52 -0500

    Update game.py

--- game.py
+++ game.py
@@ -25,6 +25,7 @@ computer_choice= random.choice(rps)
 print("The computer chose: " , computer_choice)
 print("----------------")
 
+# I added the last 9 lines of the code to allow for an uppercase first letter becasue I could not get the lower() fuction to work properly. 
 
 if computer_choice==user_input:print("it's a tie")
 elif user_input == "paper" and computer_choice == "rock": print("You win! Congrats")
@@ -34,7 +35,6 @@ elif user_input == "rock" and computer_choice == "scissors": print("You win! Con
 elif user_input == "scissors" and computer_choice == "paper": print("You win! Congrats")
 elif user_input == "scissors" and computer_choice == "rock": print("Oh! The computer won, that's ok!")
 
-# I added this part of the code to allow for an uppercase first letter becasue I could not get the lower() fuction to work properly. 
 if computer_choice==user_input:print("it's a tie")
 elif user_input == "Paper" and computer_choice == "rock": print("You win! Congrats")
 elif user_input == "Paper" and computer_choice == "scissors": print("Oh! The computer won, that's ok!")
